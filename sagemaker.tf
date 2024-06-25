06af7324ffaae03ed4157823695afbe3708c229d
Hausdorff94 <jhonyal58@gmail.com>
2024-06-25 10:14:10 -0500

    Add modules

--- sagemaker.tf
+++ sagemaker.tf
@@ -5,35 +5,20 @@ resource "aws_sagemaker_domain" "mlflow_domain" {
   subnet_ids  = var.subnet
 
   default_user_settings {
-    execution_role = aws_iam_role.mlflow_example.arn
+    execution_role = aws_iam_role.mlflow_role.arn
   }
 }
 
-resource "aws_iam_role" "mlflow_role" {
-  name               = "mlflow_role"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.mlflow_trust_policy.json
-}
-
-data "aws_iam_policy_document" "mlflow_trust_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["sagemaker.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_sagemaker_app" "example" {
+resource "aws_sagemaker_app" "mlflow_app" {
   domain_id         = aws_sagemaker_domain.mlflow_domain.id
   user_profile_name = aws_sagemaker_user_profile.mlflow_user.user_profile_name
   app_name          = "mlflow_app"
   app_type          = "JupyterServer"
+  depends_on = [ aws_sagemaker_domain.mlflow_domain ]
 }
 
 resource "aws_sagemaker_user_profile" "mlflow_user" {
   domain_id         = aws_sagemaker_domain.mlflow_domain.id
   user_profile_name = "mlflow_user"
+  depends_on = [ aws_sagemaker_app.mlflow_app ]
 }
