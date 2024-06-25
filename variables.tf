4117b890483aead794baa469bfd379c8b8893b43
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2024-06-25 15:23:37 +0000

    Just deploy IAM role

--- variables.tf
+++ variables.tf
@@ -10,3 +10,9 @@ variable "subnet" {
     type = list(string)
     default = ["value"]
 }
+
+variable "region" {
+    description = "AWS Region"
+    type = string
+    default = "us-east-1"
+}
--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,17 @@
-resource "aws_vpc" "mlflow_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "mlflow-vpc"
-  }
-}
+# resource "aws_vpc" "mlflow_vpc" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
+#   tags = {
+#     Name = "mlflow-vpc"
+#   }
+# }
 
-resource "aws_subnet" "mlflow_subnet" {
-  vpc_id     = aws_vpc.mlflow_vpc.id
-  cidr_block = "10.0.1.0/24"
+# resource "aws_subnet" "mlflow_subnet" {
+#   vpc_id     = aws_vpc.mlflow_vpc.id
+#   cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "mlflow-subnet"
-  }
-}
+#   tags = {
+#     Name = "mlflow-subnet"
+#   }
+# }
